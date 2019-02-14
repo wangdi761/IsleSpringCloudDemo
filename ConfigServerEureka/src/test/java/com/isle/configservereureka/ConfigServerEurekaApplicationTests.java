489f64dc11ab74e1b28d7b7c5e08ab1bc35fc494
19981259081 <isle_di@aliyun.com>
2019-02-14 16:57:57 +0800

    提交

---
+++ ConfigServerEureka/src/test/java/com/isle/configservereureka/ConfigServerEurekaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.isle.configservereureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigServerEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
