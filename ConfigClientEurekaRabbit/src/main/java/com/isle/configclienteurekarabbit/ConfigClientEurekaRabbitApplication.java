3d9f4812960f4020431df2f06d06191a17fd3979
wangd <wangd@fenqi.im>
2019-02-15 18:16:24 +0800

    提交

---
+++ ConfigClientEurekaRabbit/src/main/java/com/isle/configclienteurekarabbit/ConfigClientEurekaRabbitApplication.java
@@ -0,0 +1,14 @@
+package com.isle.configclienteurekarabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientEurekaRabbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientEurekaRabbitApplication.class, args);
+	}
+
+}
+
---
+++ RabbitmqHello/src/main/java/com/isle/rabbitmqhello/rabbit/Receiver.java
@@ -0,0 +1,21 @@
+package com.isle.rabbitmqhello.rabbit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RabbitListener(queues = "hello")
+public class Receiver {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
+
+    @RabbitHandler
+    public void process(String hello) {
+        LOGGER.info("Receiver: {}", hello);
+    }
+
+
+}
