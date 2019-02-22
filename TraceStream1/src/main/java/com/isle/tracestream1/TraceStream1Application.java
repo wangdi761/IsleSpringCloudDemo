05dd41ceb123b7d5a58eb19ddbf05ee1f2a34d9a
wangd <wangd@fenqi.im>
2019-02-22 19:13:52 +0800

    提交

--- TraceStream1/src/main/java/com/isle/tracestream1/TraceStream1Application.java
+++ TraceStream1/src/main/java/com/isle/tracestream1/TraceStream1Application.java
@@ -3,10 +3,19 @@ package com.isle.tracestream1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class TraceStream1Application {
+
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TraceStream1Application.class, args);
--- ZipkinServerStream/src/main/java/com/isle/zipkinserverstream/ZipkinServerStreamApplication.java
+++ ZipkinServerStream/src/main/java/com/isle/zipkinserverstream/ZipkinServerStreamApplication.java
@@ -2,7 +2,9 @@ package com.isle.zipkinserverstream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
+@EnableZipkinStreamServer
 @SpringBootApplication
 public class ZipkinServerStreamApplication {
 
